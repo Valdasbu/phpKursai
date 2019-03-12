9eb3a47a0f95d94754bfe0adf256da4d353a02cd
Valdas <48052519+Valdasbu@users.noreply.github.com>
2019-03-12 15:49:28 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,87 @@
+<?php require_once 'function.php';
+    $sekme = '';
+    $sekmeMessage = '';
+    $fullNamePranesimas = '';
+    $emailPranesimas = '';
+    $sendMessagePranesimas = '';
+
+    if(count($_POST) > 0) {
+      if(empty($_POST['fullname'])) {
+        $fullNamePranesimas = "Enter your full name";
+        $sekme=false;
+      }
+      elseif (strlen($_POST['fullname'])<3) {
+        $fullNamePranesimas = "Vardas turi būti ne mažiau kaip 3 simboliai";
+        $sekme=false;
+      }
+      elseif (!isValidName($_POST['fullname'])) {
+        $fullNamePranesimas = "Full name turi būti tik iš raidžių";
+        $sekme=false;
+      }
+
+      if(empty($_POST['email'])) {
+        $emailPranesimas = "Enter your email";
+      }
+      elseif(!isValidEmail($_POST['email'])) {
+        $emailPranesimas = "Įveskite teisingą el. pašto adresą.";
+        $sekme=false;
+      }
+
+      if(empty($_POST['sendmessage'])) {
+        $sendMessagePranesimas = "Enter your message";
+        $sekme=false;
+      }
+      elseif (strlen($_POST['sendmessage'])<=5||strlen($_POST['sendmessage'])>500) {
+        $sendMessagePranesimas = "Message turi būti ne mažiau kaip 5 ir ne daugiau kaip 500 simbolių";
+        $sekme=false;
+      }
+
+      if($sekme !== false){
+        $sekmeMessage = "Formos duomenys įvesti teisingai!!! Žinutė išsiųsta. Susisieksime su Jumis per artimiausias tris darbo dienas.";
+      }
+
+      {
+        $user = 'root';
+        $pass = '';
+
+        $db = new PDO('mysql:host=localhost;dbname=raudonas_projektas',
+                      $user,
+                      $pass,
+                      array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+        $query = $db->prepare("INSERT INTO contact (fullname, email, sendmessage) VALUES (:fullname, :email, :sendmessage)");
+
+        $query->execute([
+                        'fullname' => $_POST['fullname'],
+                        'email' => $_POST['email'],
+                        'sendmessage' => $_POST['sendmessage']
+                      ]);
+      }
+    }
+?>
+<section class="contact">
+    <div class="title">
+        <h2 id=cia>Contact Us</h2>
+    </div>
+    <div class="container flex">
+        <div class="form">
+            <form action=#cia method="post" novalidate autocomplete="on">
+                <input type="text" name="fullname" placeholder="Full name" autocomplete>
+                    <p><?=$fullNamePranesimas;?></p>
+                <input type="text" name="email" placeholder="E-mail" autocomplete>
+                <p><?=$emailPranesimas;?></p>
+                <textarea name="sendmessage" placeholder="Message" autocomplete></textarea>
+                <p><?=$sendMessagePranesimas;?></p>
+                <button type="submit">Send</button>
+                <p><?=$sekmeMessage;?></p>
+            </form>
+        </div>
+
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.2196022838775!2d25.335691251101817!3d54.72335198019391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96e7d814e149%3A0x86772a85fe5a2719!2sBaltic+Talents+Vilnius!5e0!3m2!1sen!2slt!4v1525271005315" width="400" height="260" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <p>811 7th Avenue 53rd Street New York, 10019, United States</p>
+        </div>
+    </div>
+</section>
+
+</main>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homework</title>
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700|Sintony" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+        <script type="text/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+
+          <?php require_once 'header.php'; ?>
+          <?php require_once 'premium.php'; ?>
+          <?php require_once 'team.php'; ?>
+          <?php require_once 'carousel.php'; ?>
+          <?php require_once 'contact.php'; ?>
+          <?php require_once 'footer.php'; ?>
+
+    </body>
+</html>
